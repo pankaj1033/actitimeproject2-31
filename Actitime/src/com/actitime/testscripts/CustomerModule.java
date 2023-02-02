4d7ff0141f59a67ea0f03b35a6a43083d1a244d5
pankaj1033 <119401554+pankaj1033@users.noreply.github.com>
2023-02-02 23:22:31 +0530

    first commit

---
+++ Actitime/src/com/actitime/testscripts/CustomerModule.java
@@ -0,0 +1,42 @@
+package com.actitime.testscripts;
+
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.actitime.generics.BaseClass;
+import com.actitime.generics.FileLib;
+import com.actitime.pom.HomePage;
+import com.actitime.pom.TaskListPage;
+
+@Listeners(com.actitime.generics.ListenerImplementation.class)
+public class CustomerModule extends BaseClass {
+	@Test
+	public void testCreateCustomer() throws InterruptedException, EncryptedDocumentException, IOException {
+	Reporter.log("CreateCustomer",true);
+	FileLib f=new FileLib();
+	String customerName =f.getExcelData("CreateCustomer", 1, 2);
+	String customerDescprition = f.getExcelData("CreateCustomer", 1, 3);
+	HomePage h=new HomePage(driver);
+	h.setTaskTab();
+	TaskListPage t=new TaskListPage(driver);
+	t.getAddNewBtn().click();
+	t.getNewCustomerOption().click();
+	t.getCustomerNameTbx().sendKeys(customerName);
+	t.getCustomerDescriptionTbx().sendKeys(customerDescprition);
+	t.getSelectCustomerDD().click();Thread.sleep(5000);System.out.println("Helloo");
+	t.getOurCompanyTx().click();
+	t.getCreateCustomerBtn().click();
+	WebDriverWait wait=new WebDriverWait(driver, 10);
+	wait.until(ExpectedConditions.textToBePresentInElement(t.getActualCustomer(), customerName));
+	String actualText = t.getActualCustomer().getText();
+	Assert.assertEquals(actualText, customerName);
+	Thread.sleep(5000);
+	}
+}
